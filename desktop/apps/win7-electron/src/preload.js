8935d41dea8cf3b0d23a354bb91f71da7fe66c77
binhu-dev <dev@binhu.local>
2026-09-01 12:35:18 +0800

    release: prepare v0.28.1

--- desktop/apps/win7-electron/src/preload.js
+++ desktop/apps/win7-electron/src/preload.js
@@ -3,7 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('binhuDesktop', {
   target: 'win7',
   saveFile: (filename, data) => ipcRenderer.invoke('desktop:save-file', { filename, data }),
-  appVersion: '0.28.0',
+  appVersion: '0.28.1',
   getConfig: () => ipcRenderer.invoke('desktop:get-config'),
   openOnline: () => ipcRenderer.invoke('desktop:open-online'),
   openOffline: () => ipcRenderer.invoke('desktop:open-offline'),
--- desktop/apps/win7-vxkex/installer/BinhuWin7VxKex.iss
+++ desktop/apps/win7-vxkex/installer/BinhuWin7VxKex.iss
@@ -1,8 +1,8 @@
 #ifndef AppVersion
-  #define AppVersion "0.28.0"
+  #define AppVersion "0.28.1"
 #endif
 #ifndef NumericVersion
-  #define NumericVersion "0.28.0.0"
+  #define NumericVersion "0.28.1.0"
 #endif
 #ifndef VelopackSetup
   #error VelopackSetup must point to the baseline Velopack Setup executable.
